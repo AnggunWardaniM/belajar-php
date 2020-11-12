4f90c5855e54ada2172a45c250e7e56a82afdbe8
AnggunWardaniM <3103119024@student.smktelkom-pwt.sch.id>
2020-11-12 15:22:55 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php 
+require 'functions.php';
+$siswa = query ("SELECT * FROM siswa");
+?>
+<!DOCTYPE html>
+<html>
+<head> 
+	<title>Beranda</title>
+</head>
+<body>
+	<h1>Daftar Siswa</h1>
+	<table border="1" cellpadding="10" cellspacing="0">
+		<tr>
+			<td>Nama </td>
+            <td>NIS </td>
+            <td>PASSWORD </td>
+		</tr>
+		<?php foreach ($siswa as $row) :?>
+		<tr>
+			<td><?= $row["nama"]?></td>
+            <td><?= $row["nis"]?></td>
+            <td><?= $row["password"]?></td>
+		</tr>
+		<?php endforeach; ?>
+	</table>
+</body>
+</html>
---
+++ proses.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+    <h1>Hallo,Annyeong,Hai <?= $_POST["nama"];?><br>
+        Kamu Tinggal di <?= $_POST["alamat"]; ?></h1>
+</body>
+</html>
